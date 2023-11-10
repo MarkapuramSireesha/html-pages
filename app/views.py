3c4b51efdcd9158bd8c403d7e925cb7924e1c45c
Marakapuram Sireesha <siri400@gmail.com>
2023-11-10 12:07:22 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def nandu(request):
+    return render(request,'nandu.html')
+def pandu(request):
+    return render(request,'pandu.html')
+def sam(request):
+    return render(request,'sam.html')
+def sweety(request):
+    return render(request,'sweety.html')
